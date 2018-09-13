89e553b34a32c3b65791bbe6753b5cf8552ccaca
Ahmet Can Güven <ahmetcan.guven@trendyol.com>
2018-09-13 12:39:57 +0300

    coveralls, travis tasks implemented

---
+++ test/index.spec.ts
@@ -0,0 +1,14 @@
+import {Test} from "../lib";
+import {expect} from "chai";
+
+
+describe('Library', () => {
+  it('should test', () => {
+    //Arrange
+    expect(Test).to.be.a('function');
+    //Act
+
+    //Assert
+
+  });
+});
